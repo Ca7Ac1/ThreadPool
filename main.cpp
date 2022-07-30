ece31c4fe4512ff22a659a27e0790bbe424c8f08
Ca7Ac1 <Ca7Ac1@gmail.com>
2022-07-30 18:47:34 -0400

    Test driver

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <chrono>
+
+#include "ThreadPool.hpp"
+
+void add(int a, int b)
+{
+    int c = a + b;
+}
+
+void smallChunk()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < (int)1e6; i++)
+    {
+        add(i, i % 700 * 7);
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << "Running on main thread: " << duration.count() << " milliseconds" << std::endl;
+
+    for (int threadCount = 1; threadCount <= 10; threadCount++)
+    {
+        ThreadPool pool(threadCount);
+
+        start = std::chrono::high_resolution_clock::now();
+
+        for (int i = 0; i < (int)1e6; i++)
+        {
+            pool.addTask(&add, i, i % 700 * 7);
+        }
+
+        pool.end();
+
+        stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+        std::cout << "Running on " << threadCount << " thread: " << duration.count() << " milliseconds" << std::endl;
+    }
+}
+
+void addMany(int a, int b)
+{
+    for (int i = 0; i < (int)1e6; i++)
+    {
+        int c = a + b;
+    }
+}
+
+void bigChunk()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < (int)1e3; i++)
+    {
+        addMany(i, i % 700 * 7);
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << "Running on main thread: " << duration.count() << " milliseconds" << std::endl;
+
+    for (int threadCount = 1; threadCount <= 10; threadCount++)
+    {
+        ThreadPool pool(threadCount);
+
+        start = std::chrono::high_resolution_clock::now();
+
+        for (int i = 0; i < (int)1e3; i++)
+        {
+            pool.addTask(&addMany, i, i % 700 * 7);
+        }
+
+        pool.end();
+
+        stop = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+        std::cout << "Running on " << threadCount << " thread: " << duration.count() << " milliseconds" << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout << "                 SMALL CHUNK                 " << std::endl;
+    std::cout << "_____________________________________________" << std::endl;
+    smallChunk();
+   
+    std::cout << "                  BIG CHUNK                  " << std::endl;
+    std::cout << "_____________________________________________" << std::endl;
+    bigChunk();
+
+    return 0;
+}
